aa3e451fc7856bbbba042384188cdb313d1f35da
Nawabu-Sujatha <122606574+Nawabu-Sujatha@users.noreply.github.com>
2023-08-22 17:23:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-       region = "ap-south-1"
-}
-resource "aws_s3_bucket" "sample"{
-        bucket = "terraformbucket123"
-        tags = {
-               name = "terraformbucket123"
-}
+resource "aws_vpc" "terraformvpc" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    
+    tags = {
+      Name = "sujji-vpc"
+    
+  }
 }
